a9f55365c214cc0851c867ceba3486613837a057
Pedro Vinícius de Almeida Silva Xavier <120400955+Pedrovinicius123@users.noreply.github.com>
2024-09-17 17:13:39 -0300

    Update menu.py

--- app/menu.py
+++ app/menu.py
@@ -1,5 +1,6 @@
 from backend.products import Product
 from backend.cashier import General
+import pickle
 
 # Exemplo de produtos
 products = [Product("Carro 0", 30000), Product("Louça", 200), Product("Brinquedo Hotweels", 1000)]
@@ -31,7 +32,21 @@ if __name__ == "__main__":
                             print("Credenciais incorretas!")
 
             case 2:
-                pass
+                if not current_account:
+                    print("Conta não selecionada!")
+
+                else:
+                    for product in products:
+                        print(f'{product.product_name} - {product.price}')
+                    
+                    prod = input('Escolha o produto: ')
+                    prods = list(map(lambda x:x.product_name, products))
+                    
+                    while prod not in prods:
+                        print("Produto não existente!")
+                        prod = input('Escolha o produto: ')
+                    
+                    current_account.add_item(products[prods.index(prod)])
 
             case 3:
                 username=input("Digite o nome de usuário: ")
@@ -47,5 +62,32 @@ if __name__ == "__main__":
 
                 bank_account=input("Informe o seu banco: ")
                 # Criação da instância da classe
-                accounts.append(General(username=username, age=age, email=email, saldo=saldo, senha=senha, bank_account=bank_account))
+                accounts = []
+                try:
+                    with open("backend/data.pkl", 'rb') as file:
+                        accounts = pickle.load(file)
+
+                except Exception as err:
+                    print("Armazenando dados...")
+
+                finally:
+                    accounts.append(General(username=username, age=age, email=email, saldo=saldo, senha=senha, bank_account=bank_account))
+                    with open("backend/data.pkl", 'wb') as file:
+                        pickle.dump(accounts, file)
+
                 print("Conta Logada com sucesso!")
+
+            case 4:
+                senha = input("Digite a sua senha: ")
+                if current_account.pagar(senha):
+                    while True:
+                        sub_option=input("Quer continuar comprando? [s/n] ")
+                        if sub_option.lower() == 's':
+                            break
+                        
+                        elif sub_option.lower() == 'n':
+                            option = 0
+                            break
+                
+                else:
+                    print("Operação negada!")
